85495272be0171bff2cfb87eab4144c632779e99
Josue Lopez <josue.ismael.lopez.ramirez@gmail.com>
2023-06-17 19:49:15 -0600

    commit

--- src/shared/shared.service.ts
+++ src/shared/shared.service.ts
@@ -32,7 +32,7 @@ export class SharedService {
           if (err) {
             throw err;
           }
-          console.log("Delete File successfully.");  
+          console.log("Delete File successfully.");
         });
       }, 300);
       return data;
@@ -45,34 +45,51 @@ export class SharedService {
     const backup = await this.backupsService.findOne(id);
     const file = require('fs').createWriteStream(join(process.cwd(), './temp/restore.sql'));
     (await this.awsService.getFromS3(backup.name)).pipe(file);
-    
-    const dataSql = await require('fs').readFileSync(join(process.cwd(), './temp/restore.sql')).toString();
-    
-    console.log({ dataSql });
-    
-    const dataArr = dataSql.toString().split(';');
 
-    console.log({ dataArr });
+    const dataSql = require('fs').readFileSync(join(process.cwd(), './temp/restore.sql'), 'utf8');
 
-    // dataArr.forEach((query) => {
-    //   if(query) {
-    //     // Add the delimiter back to each query before you run them
-    //     // In my case the it was `);`
-    //     query += ');';
-    //     db.run(query, (err) => {
-    //        if(err) throw err;
-    //     });
-    //   }
-    // });
+    const connection = mysql.createConnection({
+      multipleStatements: true,
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+    })
 
-    // const connection = mysql.createConnection({
-    //   multipleStatements: true,
-    //   host: process.env.DATABASE_HOST,
-    //   database: process.env.DATABASE_NAME,
-    //   port: parseInt(process.env.DATABASE_PORT, 10),
-    //   user: process.env.DATABASE_USERNAME,
-    //   password: process.env.DATABASE_PASSWORD,
-    // })
-    // connection.query(backup.url, (err) => { console.log(err ? err : 'restored!') });
+    const dropQuery = `
+    SET FOREIGN_KEY_CHECKS = 0;
+
+    SELECT CONCAT('TRUNCATE TABLE ', table_name, ';') AS sql_statement
+    FROM information_schema.tables
+    WHERE table_schema = '${process.env.DATABASE_NAME}';
+  `;
+
+    connection.query(dropQuery, (err, results) => {
+      if (err) {
+        console.error('Error dropping data:', err);
+        return;
+      }
+
+      const truncateQueries = results[1];
+
+      truncateQueries.forEach((query) => {
+        connection.query(query.sql_statement, (err) => {
+          if (err) {
+            console.error('Error truncating table:', err);
+            return;
+          }
+          console.log('Table truncated successfully:', query.sql_statement);
+        });
+      });
+
+      connection.query('SET FOREIGN_KEY_CHECKS = 1;', (err) => console.log('ERROR ENABLING FOREIGN KEY CHECKS'));
+      console.log('Data dropped successfully');
+    });
+
+    setTimeout(() => {
+      connection.query(dataSql, (err) => { console.log(err ? err : 'restored database successfuly!') });
+      connection.end();
+    }, 3000)
   }
 }
