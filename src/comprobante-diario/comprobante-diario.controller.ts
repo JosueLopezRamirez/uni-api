016ab7b83dae817a3417f54c44bf34292f5d8eb4
Josue Lopez <josue.ismael.lopez.ramirez@gmail.com>
2023-06-24 11:00:21 -0600

    commit

--- src/comprobante-diario/comprobante-diario.controller.ts
+++ src/comprobante-diario/comprobante-diario.controller.ts
@@ -8,19 +8,20 @@ import {
   Delete,
   Req,
   Query,
+  Res,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { ComprobanteDiarioService } from './comprobante-diario.service';
 import { DetalleComprobanteDto } from './dto/create-comprobante-diario.dto';
-import { UpdateComprobanteDiarioDto } from './dto/update-comprobante-diario.dto';
+import pdf from "html-pdf";
 
 @Controller('comprobante-diario')
 export class ComprobanteDiarioController {
   constructor(
     private readonly comprobanteDiarioService: ComprobanteDiarioService,
     private jwtService: JwtService,
-  ) {}
+  ) { }
 
   @Post()
   create(
@@ -65,5 +66,22 @@ export class ComprobanteDiarioController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.comprobanteDiarioService.remove(id);
+  }
+
+
+  @Get("/generate-report/:id")
+  async generateReport(@Param('id') id: string, @Res() res: Response) {
+    const pdfFile = await this.comprobanteDiarioService.generateReport(id);
+
+    pdf.create(pdfFile).toBuffer((err, buffer) => {
+      if (err) {
+        // some code
+      } else {
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename=invoice.pdf');
+
+        res.send(buffer);
+      }
+    });
   }
 }
